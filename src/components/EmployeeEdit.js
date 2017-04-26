eb7cf54995fd407b0c3fdcd3066e053af2efcde4
Scott Wilkins <scottwilkins@Scotts-mac.local>
2017-04-26 16:44:35 -0600

    add delete function

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+  state = { showmodal: false }
   componentWillMount() {
     //little trick to pre-populate the form
     _.each(this.props.employee, (value, prop) => {
@@ -21,6 +22,12 @@ class EmployeeEdit extends Component {
     const { phone, shift } = this.props;
 
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  }
+  onAccept() {
+    this.props.employeeDelete({ uid: this.props.employee.uid })
+  }
+  onDecline() {
+    this.setState({ showmodal: false });
   }
   render() {
     return (
@@ -37,6 +44,20 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showmodal: !this.state.showmodal })}>
+            Delete Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showmodal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
+          Are you sure?
+        </Confirm>
       </Card>
     );
   }
@@ -46,4 +67,8 @@ const mapStateToProps = state => {
   const { name, phone, shift } = state.employeeForm;
   return { name, phone, shift };
 };
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps, {
+    employeeUpdate,
+    employeeSave,
+    employeeDelete
+  })(EmployeeEdit);
